022fdad3c5aa65ac3733126705553a672d185a39
Rhodri Richards <rhodri.devlopment@gmail.com>
2025-03-22 14:39:08 +0000

    Initial Commit

---
+++ Push/PushApp.swift
@@ -0,0 +1,17 @@
+//
+//  PushApp.swift
+//  Push
+//
+//  Created by Rhodri Richards on 22/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct PushApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PushTests/PushTests.swift
@@ -0,0 +1,17 @@
+//
+//  PushTests.swift
+//  PushTests
+//
+//  Created by Rhodri Richards on 22/03/2025.
+//
+
+import Testing
+@testable import Push
+
+struct PushTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
